b790617a634ed58ba28a83b03601a8a5895189b6
Kyour.cn <kyour@outlook.com>
2023-07-06 18:01:53 +0800

    feat: 数据加密解密demo

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
-import "syscall/js"
+import (
+	"syscall/js"
+	"web/utils"
+)
 
 func main() {
 	//alert := js.Global().Get("alert")
 	//alert.Invoke("Hello World!")
 
 	js.Global().Set("hello", js.FuncOf(hello))
+	js.Global().Set("encryptGo", js.FuncOf(encrypt))
+	js.Global().Set("decryptGo", js.FuncOf(decrypt))
+
 	// 阻塞主线程，等待 js 调用
 	select {}
 }
@@ -19,4 +25,34 @@ func hello(this js.Value, args []js.Value) interface{} {
 	println("Hello,", name+"!")
 	// 返回 nil，表示没有返回值
 	return nil
+}
+
+// 定义一个函数，接受一个 js.Value 类型的参数
+func encrypt(this js.Value, args []js.Value) interface{} {
+	data := args[0].String()
+
+	// 密钥，长度必须为16、24或32字节
+	key := []byte("0123456789abcdef")
+
+	ciphertext, err := utils.EncryptAES(key, data)
+	if err != nil {
+		println("加密失败:", err)
+		return nil
+	}
+	return ciphertext
+}
+
+// 定义一个函数，接受一个 js.Value 类型的参数
+func decrypt(this js.Value, args []js.Value) interface{} {
+	data := args[0].String()
+
+	// 密钥，长度必须为16、24或32字节
+	key := []byte("0123456789abcdef")
+
+	ciphertext, err := utils.DecryptAES(key, data)
+	if err != nil {
+		println("加密失败:", err)
+		return nil
+	}
+	return ciphertext
 }
---
+++ utils/aes_encrypt_decrypt.go
@@ -0,0 +1,80 @@
+package utils
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/hex"
+)
+
+// EncryptAES 加密
+func EncryptAES(key []byte, plaintext string) (string, error) {
+
+	// 创建一个AES加密块
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// 创建一个加密模式
+	mode := cipher.NewCBCEncrypter(block, key[:aes.BlockSize])
+
+	plaintextBytes := []byte(plaintext)
+
+	// 对明文进行填充
+	plaintextBytes = PKCS7Padding(plaintextBytes, aes.BlockSize)
+
+	// 创建一个字节数组用于存储加密后的密文
+	ciphertext := make([]byte, len(plaintextBytes))
+
+	// 加密明文
+	mode.CryptBlocks(ciphertext, plaintextBytes)
+
+	// 将密文转换为十六进制字符串
+	ciphertextHex := hex.EncodeToString(ciphertext)
+
+	return ciphertextHex, nil
+}
+
+// DecryptAES 解密
+func DecryptAES(key []byte, ciphertextHex string) (string, error) {
+
+	// 将密文转换为字节数组
+	ciphertext, err := hex.DecodeString(ciphertextHex)
+	if err != nil {
+		panic(err)
+	}
+
+	// 创建一个AES解密块
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// 创建一个解密模式
+	mode := cipher.NewCBCDecrypter(block, key[:aes.BlockSize])
+
+	// 创建一个字节数组用于存储解密后的明文
+	plaintext := make([]byte, len(ciphertext))
+
+	// 解密密文
+	mode.CryptBlocks(plaintext, ciphertext)
+
+	// 去除填充
+	plaintext = PKCS7UnPadding(plaintext)
+
+	return string(plaintext), nil
+}
+
+// PKCS7Padding 对明文进行填充
+func PKCS7Padding(data []byte, blockSize int) []byte {
+	padding := blockSize - len(data)%blockSize
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(data, padText...)
+}
+
+// PKCS7UnPadding 去除填充
+func PKCS7UnPadding(data []byte) []byte {
+	padding := int(data[len(data)-1])
+	return data[:len(data)-padding]
+}
